0dbfaf3d382d8a87d9acec31d68c5ccf3eb00cbf
Richard Cotton <33453953+JeffCottonBWFC@users.noreply.github.com>
2021-05-24 23:58:58 +0100

    Update __Problem120 (Square Remainders).cpp

--- SolutionsFile/__Problem120 (Square Remainders).cpp
+++ SolutionsFile/__Problem120 (Square Remainders).cpp
@@ -3,11 +3,11 @@
 
 //Problem 120 (COMPLETE)
 //Let r be the remainder when (a−1)^n + (a+1)^n is divided by a^2.
-//For example, if a = 7 and n = 3, then r = 42: 63 + 83 = 728 ≡ 42 mod 49. And as n varies, so too will r, but for a = 7 it turns out that r_max = 42.
+//For example, if a = 7 and n = 3, then r = 42: 6^3 + 8^3 = 728 ≡ 42 mod 49. And as n varies, so too will r, but for a = 7 it turns out that r_max = 42.
 //For 3 ≤ a ≤ 1000, find ∑ r_max.
 
 //NOTES
-//o Varing values of n for fixed a into equation results in r = 2 for n even and 2an for n odd.
+//o Varing values of n for fixed a into equation results in r = 2 for n even and r = 2an for n odd.
 //o The maximum n such that 2n is closest to a is when 2n_max = a-1. Thus n_max = (a-1)/2. n_max must be integer so use floor fn (or integer division).
 
 void SquareRemainders(int SummationMin, int SummationMax){
